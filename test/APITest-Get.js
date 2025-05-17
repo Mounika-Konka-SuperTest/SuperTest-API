41c74ce0a5c9dc3f160574e5a746a781ca5ade8f
Mounika-Konka-SuperTest <mounikak9949@gmail.com>
2025-05-17 20:24:49 +0530

    Initial project commit

---
+++ test/APITest-Get.js
@@ -0,0 +1,31 @@
+import supertest from 'supertest';
+import { expect } from 'chai';
+const request = supertest('https://gorest.co.in/public/')
+const TOKEN = '527246668dcc146507572d9f41a43963bf066f080f8b7fea9c9b2b989d040d6b';
+
+describe('API - users', () => {
+    // it('GET request test', () => {
+    //     // request.get(
+    //     //     `v2/users?access-token=${TOKEN}`).end((err, res) => {
+    //     //     console.log(res.body); // Debug the response
+    //     //     expect(res.status).to.equal(200);
+    //     //     //expect(res.body).to.have.property('data');
+    //     //     expect(res.body.data).to.not.be.empty;
+    //     //     done();
+    //     // });
+    //     return request.get(
+    //         `v2/users?access-token=${TOKEN}`).end((res) => {
+            
+    //         expect(res.body.data).to.not.be.empty;
+            
+    // });
+     it('GET /users/ID test', () => {
+         request.get(
+            `v2/users/7896432?access-token=${TOKEN}`).then((res) => {
+            console.log(res.body); 
+            expect(res.body.data.id).to.be.equal(7896432);
+            
+        });
+    });
+     
+});
